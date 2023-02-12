42796d0a295f9c8591c7baf7b07ba43c949e0e0a
Vishal Agarwal <vishalagarwal5720@gmail.com>
2023-02-12 13:36:08 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [state, setState] = useState(1);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const get = await fetch(`https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`);
+
+      const res = await get.json();
+
+      setData(res);
+      console.log(res);
+    }
+    getData();
+
+    document.title = `(${state}) Employees Online`;
+
+  }, [state])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className='btnandemploy'>
+      <button onClick={() => setState(state+1)} type="button" class="btn btn-primary mt-3">Get Employees </button>
+      <div className='employee'>Current Online Employees: {state}</div>
+      </div>
+      {
+        data.map((element, index) => {
+          return(
+            <div className='data'>
+              <h4>{element.firstName}</h4>
+              <h4>{element.lastName}</h4>
+              <h4>{element.email}</h4>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
